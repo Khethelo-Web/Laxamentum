95d62cab5e1c50a5daaa884e9f2c91f2e5f8ce9b
Khethelo-Web <157377663+Khethelo-Web@users.noreply.github.com>
2024-10-02 13:33:52 +0200

    Add files via upload

---
+++ components/level.js
@@ -0,0 +1,38 @@
+// SIDEBAR TOGGLE
+
+var addPrompt = document.querySelector('.addPrompt-container');
+var btnAddPrompt = document.querySelector('.addPrompt');
+var btnViewPrompt = document.querySelector('.ViewPrompt');
+
+var barChart = document.querySelector('.bar-chart');
+var btnEfficiency = document.querySelector('.efficiency');
+var btnAutomation = document.querySelector('.automation');
+var btnAnalytical = document.querySelector('.analytical');
+
+const box = document.querySelector('.box');
+const notification = document.querySelector('.notification');
+let toggle = "true";
+
+notification.addEventListener('click', () => {
+
+  if (toggle === "true") {
+    toggle = "false"
+    box.classList.add("active");
+  } else {
+    toggle = "true";
+    box.classList.remove("active");
+  }
+
+  
+  
+})
+
+
+function closeSidebar() {
+  if(addPromptOpen) {
+    addPrompt.classList.remove("active");
+    addPromptOpen = false;
+  }
+}
+
+
