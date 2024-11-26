81b012f30695e880d9e7c512c30c621001e742a6
Khethelo-Web <157377663+Khethelo-Web@users.noreply.github.com>
2024-11-26 21:07:26 +0200

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,89 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+
+const btnComputer = document.getElementById('btnComputer');
+const btnAnalytics = document.getElementById('btnAnalytics');
+const btnAdministrative = document.getElementById('btnAdministrative');
+const btnProgramming = document.getElementById('btnProgramming');
+const btnFinance = document.getElementById('btnFinance');
+const btnSales = document.getElementById('btnSales');
+const box = document.querySelector('.box');
+const notification = document.querySelector('.notification');
+let toggle = "true";
+
+notification.addEventListener('click', () => {
+
+  if (toggle === "true") {
+    toggle = "false"
+    box.classList.add("active");
+  } else {
+    toggle = "true";
+    box.classList.remove("active");
+  }
+})
+
+btnAdministrative.addEventListener('click', () => {
+  localStorage.setItem("Key", "Administrative");
+  localStorage.setItem("Level", "Beginner");
+  window.location.href = "/level.html";
+});
+
+btnFinance.addEventListener('click', () => {
+  localStorage.setItem("Key", "Finance");
+  localStorage.setItem("Level", "Beginner");
+  window.location.href = "/level.html";
+});
+
+btnSales.addEventListener('click', () => {
+  localStorage.setItem("Key", "Sales");
+  localStorage.setItem("Level", "Beginner");
+  window.location.href = "/level.html";
+});
+
+btnProgramming.addEventListener('click', () => {
+  localStorage.setItem("Key", "Programming");
+  localStorage.setItem("Level", "Beginner");
+  window.location.href = "/level.html";
+});
+
+
+const btnSend = document.querySelector('.btnSend');
+const counter = document.querySelector('.counter');
+
+let count = 0;
+
+btnSend.addEventListener('click', () => {
+  count++
+  counter.innerHTML = count;
+  Notification.requestPermission().then(perm => {
+    if (perm === "granted"){
+      const notification = new Notification("Example notification", {
+        body: "This is more text",
+        data: {hello: "World" }
+      })
+
+      notification.addEventListener("error", e => {
+        alert(error);
+      })
+
+    }
+  })
+})
---
+++ components/level.js
@@ -0,0 +1,103 @@
+// SIDEBAR TOGGLE
+
+var addPrompt = document.querySelector('.addPrompt-container');
+var btnAddPrompt = document.querySelector('.addPrompt');
+var btnViewPrompt = document.querySelector('.ViewPrompt');
+
+var barChart = document.querySelector('.bar-chart');
+var btnEfficiency = document.querySelector('.efficiency');
+var btnAutomation = document.querySelector('.automation');
+var btnAnalytical = document.querySelector('.analytical');
+
+const box = document.querySelector('.box');
+const notification = document.querySelector('.notification');
+let toggle = "false";
+
+const sideMenu = document.querySelector('.SideMenu');
+const title = document.querySelector('.header-title');
+const mainTitle = document.querySelector('.main-title');
+
+const Admin = document.getElementById('Admin_Level');
+const Finance = document.getElementById('Finance_Level');
+const btnAdmin = document.getElementById('btnAdmin');
+const btnFinance = document.getElementById('btnFinance');
+const level = document.querySelector('.level');
+const icon = document.querySelector('.icon');
+const level_container = document.querySelector('.level-container');
+const level_list = document.querySelector('.level-list');
+
+
+
+title.addEventListener('click', () => {
+
+  if (toggle === "true") {
+    toggle = "false";
+    sideMenu.classList.add("open");
+    mainTitle.classList.add("active");
+    level_container.classList.add("open");
+  } else {
+    toggle = "true";
+    sideMenu.classList.remove("open");
+    mainTitle.classList.remove("active");
+    level_container.classList.remove("open");
+  }
+  
+});
+
+btnAdmin.addEventListener('click', () => {
+
+  console.log(Admin.children[1].children[0])
+  var btnBeginner = Admin.children[1].children[0];
+  var btnMedium = Admin.children[1].children[1];
+  var btnDifficult = Admin.children[1].children[2];
+  btnBeginner.addEventListener('click', () => {
+    console.log("This is Beginner");
+  });
+  btnMedium.addEventListener('click', () => {
+    console.log("This is Medium");
+  });
+  btnDifficult.addEventListener('click', () => {
+    console.log("This is Difficult");
+  });
+
+  if (toggle === "true") {
+    toggle = "false"
+    Admin.children[1].classList.add("open");
+    Admin.children[0].children[0].classList.add("open");
+  } else {
+    toggle = "true";
+    Admin.children[1].classList.remove("open");
+    Admin.children[0].children[0].classList.remove("open");
+  }
+
+});
+
+btnFinance.addEventListener('click', () => {
+
+  if (toggle === "true") {
+    toggle = "false"
+    Finance.children[1].classList.add("open");
+    Finance.children[0].children[0].classList.add("open");
+  } else {
+    toggle = "true";
+    Finance.children[1].classList.remove("open");
+    Finance.children[0].children[0].classList.remove("open");
+  }
+
+});
+
+
+function closeSidebar() {
+  if(addPromptOpen) {
+    addPrompt.classList.remove("active");
+    addPromptOpen = false;
+  }
+}
+
+
+
+
+
+
+
+
