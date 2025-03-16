3f72a7ce11eb3c0c4cbacf43c30d9857804c133d
Khethelo-Web <157377663+Khethelo-Web@users.noreply.github.com>
2025-03-16 18:55:37 +0200

    Add files via upload

---
+++ components/logIn.js
@@ -0,0 +1,38 @@
+
+
+//let btnSignup = document.getElementById('btnSignUp');
+//let btnLogin = document.getElementById('btnLogIn');
+//let nameField = document.getElementById('nameField');
+//const username =document.getElementById('username');
+//let title = document.getElementById('title');
+//let variable;
+
+
+///btnLogin.onclick = function(){
+ ///   nameField.style.maxHeight = "0";
+ ///   title.innerHTML = "Log In";
+ ///   btnLogin.classList.remove("disabled");
+ ///   btnSignup.classList.add("disabled");
+///    variable= "down";
+ ///   if (variable == "down"){
+ ///       btnLogin.classList.add("disabled");
+ ///       btnLogin.addEventListener('click', () => {
+ ///           window.location.href="/index.html"
+ ///       })
+ ///   }
+///}
+
+///btnSignup.onclick = function(){
+///    nameField.style.maxHeight = "400px";
+ ///   title.innerHTML = "Sign Up";
+ ///   btnLogin.classList.add("disabled");
+///    btnSignup.classList.remove("disabled");
+///    variable= "up";
+///    if (variable == "up"){
+///        btnSignup.classList.add("disabled");
+ ///       btnSignup.addEventListener('click', () => {
+///            window.location.href="/index.html"
+ ///       })
+ ///   }
+///}
+
